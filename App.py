3201a4645f7a3d5d7ac37a1622f1e231cd87237d
Rishik Sai Santhosh <130997056+SaiSanthosh1508@users.noreply.github.com>
2024-09-21 19:07:58 +0530

    Update App.py

--- App.py
+++ App.py
@@ -19,12 +19,12 @@ st.divider()
 # Option to download model data images
 st.subheader('Download the images the model has trained on')
 
-gdown.download("https://drive.google.com/uc?id=11R-D1robYjdyMZ4KooWuAlZH1WyRpCdl",output="BrainTumor_1.zip")
+
 # gdown.download("https://drive.google.com/file/d/1LvSKvWVLMu11lD8cly-4Fj5aboQPdmGr/view?usp=sharing",output = "BrainTumor_1.zip")
-with open("BrainTumor_1.zip","rb") as f:
-         st.download_button(label='download data',
-                           data = f,
-                           mime='application/zip')
+# with open("BrainTumor_1.zip","rb") as f:
+#          st.download_button(label='download data',
+#                            data = f,
+#                            mime='application/zip')
 
 
 st.divider()
