fbeed5ec4f22486a59385892ca7540d4cc150752
Rishik Sai Santhosh <130997056+SaiSanthosh1508@users.noreply.github.com>
2024-09-21 19:25:19 +0530

    Update App.py

--- App.py
+++ App.py
@@ -32,7 +32,7 @@ data_df = pd.DataFrame(data={'Glioma':5284,'Meningioma':5356,"No Tumour":5828,"P
 
 if on:
     st.bar_chart(data=data_df,color=['#06C','#4CB140','#009596','#F0AB00'],stack=False)
-
+st.header(' ')
 st.header('Upload the image')
 file = st.file_uploader(label='Image file',
                  label_visibility='hidden'
