c6324ae648805e5c734572ea0b13e58f5f3f84da
Rishik Sai Santhosh <130997056+SaiSanthosh1508@users.noreply.github.com>
2024-09-21 22:01:57 +0530

    Update Ensemble_functions.py

--- Ensemble_functions.py
+++ Ensemble_functions.py
@@ -9,6 +9,7 @@ def image_preprocess(image):
     image = tf.convert_to_tensor(image, dtype=tf.float32)
     if(len(image.shape) == 2){
         image = tf.expand_dims(image,axis=-1)
+    }
     # If the image is grayscale, convert to RGB
     if image.shape[-1] == 1:  # Check if it's grayscale
         image = tf.image.grayscale_to_rgb(image)
