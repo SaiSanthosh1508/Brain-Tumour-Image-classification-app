7fd8a41c9201b797cfb0e9e99d868b7ddd18de7a
Rishik Sai Santhosh <130997056+SaiSanthosh1508@users.noreply.github.com>
2024-09-27 21:50:55 +0530

    Update App.py

--- App.py
+++ App.py
@@ -12,7 +12,7 @@ st.title('🧠 NeuroClassify')
 st.write('An approach that incorporates the power of deep learning Ensembles to identify Brain Tumours from the images')
 st.subheader("Description")
 st.write("")
-col1,col2,col3,col4 = st.columns([1,3,3,1])
+col1,col2,col3,col4 = st.columns([1,4,4,1])
 # Glioma
 with col2:
     with st.expander("1. Glioma"):
