5dde49f8259cb60af1825f0c781e0f3a39259643
Rishik Sai Santhosh <130997056+SaiSanthosh1508@users.noreply.github.com>
2024-09-27 21:17:00 +0530

    Update App.py

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ from PIL import Image
 import gdown
 
 st.title('🧠 NeuroClassify')
-st.text('An approach that incorporates the power of deep learning Ensembles to identify Brain Tumours from the images')
+st.write('An approach that incorporates the power of deep learning Ensembles to identify Brain Tumours from the images')
 st.write(" ")
 st.subheader("Description")
 
